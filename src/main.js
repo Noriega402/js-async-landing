774aaba809687663a1ec42341fca9d73bddf51b8
Noriega402 <d.noriegachajon@gmail.com>
2022-09-29 20:53:58 -0600

    Modify colors, icons, styles and scrolling

--- src/main.js
+++ src/main.js
@@ -25,15 +25,15 @@ async function fetchData(url) {
         const videos = await fetchData(API);
         //https://www.youtube.com/watch?v=OwSCzRMSgsI
         let card = `${videos.items.map(video => `
-                <a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank" rel="noopener noreferrer">
+                <a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank" rel="noopener noreferrer" class="video">
                     <div class="group relative">
                         <div
-                            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none ">
                             <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
                         </div>
                         <div class="mt-4 flex justify-between">
-                            <h3 class="text-sm text-gray-700">
-                                <span aria-hidden="true" class="absolute inset-0"></span>
+                            <h3 class="text-sm text-slate-100">
+                                <span aria-hidden="true" class="absolute inset-0 "></span>
                                 ${video.snippet.title}
                             </h3>
                         </div>
