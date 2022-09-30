985217963563c6926c058deac3f014040e4c2272
Noriega402 <d.noriegachajon@gmail.com>
2022-09-29 19:59:34 -0600

    Add tag into template card to <a> redirect a video in youtube

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCanMxWvOoiwtjL
 const content = document.getElementById('content');
 const profile = document.getElementById('img-profile');
 
-profile.setAttribute('src','https://scontent-gua1-1.xx.fbcdn.net/v/t1.6435-9/161437376_199791038535325_1848588809446346346_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=e3f864&_nc_ohc=_mwCjouTST4AX-UZIKY&_nc_ht=scontent-gua1-1.xx&oh=00_AT_4gHQLMEnr6t6YILipEiFf9u-TU1EVE8XfAYI84Tmfig&oe=635B3BE8');
+profile.setAttribute('src', 'https://scontent-gua1-1.xx.fbcdn.net/v/t1.6435-9/161437376_199791038535325_1848588809446346346_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=e3f864&_nc_ohc=_mwCjouTST4AX-UZIKY&_nc_ht=scontent-gua1-1.xx&oh=00_AT_4gHQLMEnr6t6YILipEiFf9u-TU1EVE8XfAYI84Tmfig&oe=635B3BE8');
 
 const options = {
     method: 'GET',
@@ -23,20 +23,23 @@ async function fetchData(url) {
 (async () => {
     try {
         const videos = await fetchData(API);
+        //https://www.youtube.com/watch?v=OwSCzRMSgsI
         let card = `${videos.items.map(video => `
-                <div class="group relative">
-                    <div
-                        class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                        <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+                <a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank" rel="noopener noreferrer">
+                    <div class="group relative">
+                        <div
+                            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+                        </div>
+                        <div class="mt-4 flex justify-between">
+                            <h3 class="text-sm text-gray-700">
+                                <span aria-hidden="true" class="absolute inset-0"></span>
+                                ${video.snippet.title}
+                            </h3>
+                        </div>
                     </div>
-                    <div class="mt-4 flex justify-between">
-                        <h3 class="text-sm text-gray-700">
-                            <span aria-hidden="true" class="absolute inset-0"></span>
-                            ${video.snippet.title}
-                        </h3>
-                    </div>
-                </div>
-            `).slice(0,8).join('')}
+                </a>
+            `).slice(0, 8).join('')}
         `; // .slice().join() utiliza solo 8 datos y los une
         content.innerHTML = card;
     } catch (error) {
